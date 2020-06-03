82a0076ea33bf914553aa9d9df114c9c2b84ba3e
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-06-03 21:14:16 +0200

    register Activity created

---
+++ app/src/main/java/mihaela/claudia/diosan/gsoc2020_homelessaidpanoramicinteractivesystem/register/RegisterDonorActivity.java
@@ -0,0 +1,16 @@
+package mihaela.claudia.diosan.gsoc2020_homelessaidpanoramicinteractivesystem.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mihaela.claudia.diosan.gsoc2020_homelessaidpanoramicinteractivesystem.R;
+
+public class RegisterDonorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_donor);
+    }
+}
